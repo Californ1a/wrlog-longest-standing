02232c458c75cf8a2d927e9972d0d44f23e303ba
Californ1a <rk_california@yahoo.com>
2023-07-03 02:43:25 -0400

    hb+hg

--- src/components/RecordLine.vue
+++ src/components/RecordLine.vue
@@ -1,14 +1,14 @@
 <template>
-	<tr :class="isOfficial">
+	<tr :class="officialClass">
 		<td>
 			<div class="img-container">
-				<a v-if="record.workshop_item_id !== '[Official]'" :href="workshopLink">
+				<a v-if="!isOfficial" :href="links.workshop">
 					<img :src="record.map_preview" width="64" height="36" loading="lazy" @error="hideImage" />
 				</a>
 			</div>
 		</td>
 		<td>
-			<a v-if="record.workshop_item_id !== '[Official]'" :href="workshopLink">
+			<a v-if="!isOfficial" :href="links.workshop">
 				{{ record.map_name }}
 			</a>
 			<span v-else>
@@ -17,21 +17,25 @@
 		</td>
 		<td>{{ record.mode }}</td>
 		<td>
-			<a v-if="record.workshop_item_id !== '[Official]'" :href="authorLink">
-				{{ record.author }}
-			</a>
+			<div v-if="!isOfficial">
+				<a :href="links.author">
+					{{ record.author }}
+				</a>
+				<sup> (<a :href="links.authorHB">HB</a><b> • </b><a :href="links.authorHG">HG</a>)</sup>
+			</div>
 			<span v-else>
 				[Official]
 			</span>
 		</td>
 		<td>{{ record.record_new }}</td>
 		<td>
-			<a :href="wrHolderLink">
+			<a :href="links.wrHolder">
 				{{ record.new_recordholder }}
 			</a>
+			<sup> (<a :href="links.wrHolderHB">HB</a><b> • </b><a :href="links.wrHolderHG">HG</a>)</sup>
 		</td>
 		<td>
-			<span :title="record.standingForMaxStr">{{ record.standingForStr }}</span>
+			<span :title="record.standingForMaxStr">{{ standingForDisplay }}</span>
 		</td>
 	</tr>
 </template>
@@ -40,21 +44,26 @@
 import { computed } from 'vue';
 const props = defineProps(['record']);
 
-const workshopLink = computed(() => {
-	return `https://steamcommunity.com/sharedfiles/filedetails/?id=${props.record.workshop_item_id}`;
-});
+const links = computed(() => ({
+	workshop: `https://steamcommunity.com/sharedfiles/filedetails/?id=${props.record.workshop_item_id}`,
+	author: `https://steamcommunity.com/profiles/${props.record.steam_id_author}/myworkshopfiles/?appid=233610`,
+	wrHolder: `https://steamcommunity.com/profiles/${props.record.steam_id_new_recordholder}`,
+	authorHB: `http://holdboost.com/Player?steamID=${props.record.steam_id_author}`,
+	wrHolderHB: `http://holdboost.com/Player?steamID=${props.record.steam_id_new_recordholder}`,
+	authorHG: `https://holdgrip.seekr.pw/player/${props.record.steam_id_author}/sprint`,
+	wrHolderHG: `https://holdgrip.seekr.pw/player/${props.record.steam_id_new_recordholder}/sprint`,
+}));
 
-const authorLink = computed(() => {
-	return `https://steamcommunity.com/profiles/${props.record.steam_id_author}/myworkshopfiles/?appid=233610`;
-});
-
-const wrHolderLink = computed(() => {
-	return `https://steamcommunity.com/profiles/${props.record.steam_id_new_recordholder}`;
-});
-
-const isOfficial = computed(() => ({
+const officialClass = computed(() => ({
 	official: props.record.workshop_item_id === '[Official]',
 }));
+
+const isOfficial = computed(() => props.record.workshop_item_id === '[Official]');
+
+const standingForDisplay = computed(() => {
+	const [num, unit] = props.record.standingForStr.split(' ');
+	return `${parseInt(num).toLocaleString()} ${unit}`;
+});
 
 // function removeLine(event) {
 // 	event.target.closest('tr').remove();
@@ -112,5 +121,9 @@ td:last-child span {
 	height: 36px;
 	width: 64px;
 	padding: 2px;
+}
+
+sup {
+	font-size: 0.75em;
 }
 </style>
